ae2d99dac7262b5daff3be993d97f1dc060a5f86
digitalchild <digitalchild@gmail.com>
2014-11-04 22:25:30 +1000

    Fixing attribute bug #48

--- WCVendors/classes/admin/settings/sf-options.php
+++ WCVendors/classes/admin/settings/sf-options.php
@@ -71,7 +71,7 @@ $options[ ] = array(
 		'inventory'      => 'Inventory',
 		'shipping'       => 'Shipping',
 		'linked_product' => 'Linked Products',
-		'attributes'     => 'Attributes',
+		'attribute'     => 'Attributes',
 		'advanced'       => 'Advanced',
 	),
 	'type'     => 'checkbox',
