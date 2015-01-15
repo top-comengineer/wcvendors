d84a118ca9c77385c1f291e320e3573dd0f8016d
digitalchild <digitalchild@gmail.com>
2015-01-16 08:37:53 +1000

    Version bump

--- class-wc-vendors.php
+++ class-wc-vendors.php
@@ -7,7 +7,7 @@
  * Author:              WC Vendors
  * Author URI:          http://wcvendors.com
  *
- * Version:             1.3.1
+ * Version:             1.4.0
  * Requires at least:   4.0.0
  * Tested up to:        4.1.0
  *
