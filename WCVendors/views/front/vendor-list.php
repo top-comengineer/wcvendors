44b277ab155bcbbf5cbdb348470d6d084d078682
digitalchild <digitalchild@gmail.com>
2015-01-15 17:22:38 +1000

    Added template comments

--- WCVendors/views/front/vendor-list.php
+++ WCVendors/views/front/vendor-list.php
@@ -1,3 +1,13 @@
+<?php 
+/*
+*	Template Variables available 
+*   $shop_name : pv_shop_name
+*   $shop_description : pv_shop_description (completely sanitized)
+*   $shop_link : the vendor shop link 
+*   $vendor_id  : current vendor id for customization 
+*/
+?>
+
 <li>
 	<a href="<?php echo $shop_link; ?>" alt="<?php echo $shop_name; ?>" >
 	    <figure>
