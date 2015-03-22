6f99043da11527eb79f324c8d6d5d26f76e35379
digitalchild <digitalchild@gmail.com>
2015-03-23 09:52:14 +1000

    Fixed index issue #122

--- classes/admin/class-admin-users.php
+++ classes/admin/class-admin-users.php
@@ -52,7 +52,7 @@ class WCV_Admin_Users
 			// WC > Product featured
 			$product_misc  = (array) WC_Vendors::$pv_options->get_option( 'hide_product_misc' );
 			
-			if ($product_misc['featured']) { 
+			if ( isset( $product_misc['featured'] ) ) { 
 				add_filter( 'manage_product_posts_columns', array($this, 'manage_product_columns'), 99);
 			}
 			
