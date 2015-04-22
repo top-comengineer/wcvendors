78d856a332b05f4639a65522ac12d458411ec896
digitalchild <digitalchild@gmail.com>
2015-04-22 12:15:57 +1000

    Commenced work on new vendor dashboard pages

---
+++ classes/admin/class-vendor-page.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ *  WC Vendor Page - Vendor WP-Admin Dashboard
+ * 
+ * @author Jamie Madden <http://wcvendors.com>
+ * @package WCVendors
+ */
+
+Class WCV_Vendor_page { 
+
+	function __construct(){ 
+		add_action( 'admin_menu', array( $this, 'settings_page_admin_menu') ); 
+	}
+
+
+	function settings_page_admin_menu(){
+        add_menu_page( __('Shop Settings', 'wcvendors'), __('Shop Settings', 'wcvendors'), 'manage_options', 'test-plugin', 'settings_page' );
+	}
+ 
+	function settings_page(){ 
+		echo '<h1>Shop Settings</h1>'; 
+	}
+
+}
