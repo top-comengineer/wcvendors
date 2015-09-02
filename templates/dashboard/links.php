a81c05e7f9dcd7c03259ff8a8ec9819168862e8c
bentasm1 <benmartinoh@gmail.com>
2015-09-02 18:08:50 -0400

    #195 Updated: Templates now fully translatable

--- templates/dashboard/links.php
+++ templates/dashboard/links.php
@@ -1,11 +1,11 @@
 <center>
 <p>
-        <a href="<?php echo $shop_page; ?>" class="button">View Your Store</a>
-        <a href="<?php echo $settings_page; ?>" class="button">Store Settings</a>
+        <a href="<?php echo $shop_page; ?>" class="button"><?php echo _e( 'View Your Store', 'wcvendors' ); ?></a>
+        <a href="<?php echo $settings_page; ?>" class="button"><?php echo _e( 'Store Settings', 'wcvendors' ); ?></a>
 
 <?php if ( $can_submit ) { ?>
-                <a target="_TOP" href="<?php echo $submit_link; ?>" class="button">Add New Product</a>
-                <a target="_TOP" href="<?php echo $edit_link; ?>" class="button">Edit Products</a>
+                <a target="_TOP" href="<?php echo $submit_link; ?>" class="button"><?php echo _e( 'Add New Product', 'wcvendors' ); ?></a>
+                <a target="_TOP" href="<?php echo $edit_link; ?>" class="button"><?php echo _e( 'Edit Products', 'wcvendors' ); ?></a>
 <?php } ?>
 </center>
 
