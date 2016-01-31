157b8f129f5f92bbda9ef3d6cc609365fee7d7d3
digitalchild <digitalchild@gmail.com>
2016-01-31 16:49:30 +1000

    Fixed: Issue with paypal loading classes incorrectly

--- classes/gateways/PayPal_AdvPayments/PayPal_AP/classes/adaptivepayments-sdk/lib/auth/PPAuth.php
+++ classes/gateways/PayPal_AdvPayments/PayPal_AP/classes/adaptivepayments-sdk/lib/auth/PPAuth.php
@@ -1,38 +1,4 @@
 <?php
-//PayPal specific modification starts
-//Method to be called for generating signature
-require_once( "AuthUtil.php" );
-class AuthSignature
-{
-
-	public function genSign( $key, $secret, $token, $tokenSecret, $httpMethod, $endpoint )
-	{
-
-
-		$authServer  = new OAuthServer( new MockOAuthDataStore() );
-		$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
-		$authServer->add_signature_method( $hmac_method );
-
-		$sig_method   = $hmac_method;
-		$authConsumer = new OAuthConsumer( $key, $secret, null );
-		$authToken    = null;
-		$authToken    = new OAuthToken( $token, $tokenSecret );
-
-		//$params is the query param array which is required only in the httpMethod is "GET"
-
-		$params = array();
-		//set the Query parameters to $params if httpMethod is "GET"
-
-		$acc_req = OAuthRequest::from_consumer_and_token( $authConsumer, $authToken, $httpMethod, $endpoint, $params );
-
-		$acc_req->sign_request( $sig_method, $authConsumer, $authToken );
-		$response = OAuthutil::parseQueryString( $acc_req );
-
-		return $response;
-
-	}
-}
-
 //PayPal specific modification ends
 /* Generic exception class
  */
@@ -1073,4 +1039,39 @@ if ( !class_exists( 'OAuthUtil' ) ) {
 		}
 	}
 }
+//PayPal specific modification starts
+//Method to be called for generating signature
+require_once( "AuthUtil.php" );
+class AuthSignature
+{
+
+	public function genSign( $key, $secret, $token, $tokenSecret, $httpMethod, $endpoint )
+	{
+
+
+		$authServer  = new OAuthServer( new MockOAuthDataStore() );
+		$hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
+		$authServer->add_signature_method( $hmac_method );
+
+		$sig_method   = $hmac_method;
+		$authConsumer = new OAuthConsumer( $key, $secret, null );
+		$authToken    = null;
+		$authToken    = new OAuthToken( $token, $tokenSecret );
+
+		//$params is the query param array which is required only in the httpMethod is "GET"
+
+		$params = array();
+		//set the Query parameters to $params if httpMethod is "GET"
+
+		$acc_req = OAuthRequest::from_consumer_and_token( $authConsumer, $authToken, $httpMethod, $endpoint, $params );
+
+		$acc_req->sign_request( $sig_method, $authConsumer, $authToken );
+		$response = OAuthutil::parseQueryString( $acc_req );
+
+		return $response;
+
+	}
+}
+
+
 ?>
