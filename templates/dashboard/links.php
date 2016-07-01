58c44039f2f32ef1a71285c16af2ec8374499447
shanebp <shanebp@server>
2016-07-01 16:03:03 -0700

    added hook

--- templates/dashboard/links.php
+++ templates/dashboard/links.php
@@ -6,7 +6,9 @@
 <?php if ( $can_submit ) { ?>
                 <a target="_TOP" href="<?php echo $submit_link; ?>" class="button"><?php echo _e( 'Add New Product', 'wcvendors' ); ?></a>
                 <a target="_TOP" href="<?php echo $edit_link; ?>" class="button"><?php echo _e( 'Edit Products', 'wcvendors' ); ?></a>
-<?php } ?>
+<?php } 
+do_action( 'wcvendors_after_links' );
+?>
 </center>
 
 <hr>
