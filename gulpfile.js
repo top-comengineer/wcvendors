9fde86476a922cb55668719c6306adaee884579f
digitalchild <digitalchild@gmail.com>
2016-09-29 11:34:06 +0700

    Added: Automated language file builds

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// Load the dependencies 
+var gulp = require('gulp'),
+    wpPot = require('gulp-wp-pot'), 
+    sort = require('gulp-sort');
+
+// i18n files 
+gulp.task('build-i18n-pot', function () {
+    return gulp.src([ '*.php' ] )
+        .pipe( sort() )
+        .pipe( wpPot( {
+            domain: 'wcvendors',
+            destFile:'default.pot',
+            package: 'wcvendors',
+            bugReport: 'https://www.wcvendors.com',
+            lastTranslator: 'Jamie Madden <support@wcvendors.com>',
+            team: 'WC Vendors <support@wcvendors.com>'
+        } ) )
+        .pipe( gulp.dest('languages') );
+});
+
+
+gulp.task('default', [ 'build-i18n-pot' ] );
