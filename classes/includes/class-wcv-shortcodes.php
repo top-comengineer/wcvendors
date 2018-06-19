a2d413faa32ff37cb3e179bcf8a84895de7e514c
Lindeni Mahlalela <mahlamusa@gmail.com>
2018-06-19 16:37:22 +0200

    Top rated shortcode

--- classes/includes/class-wcv-shortcodes.php
+++ classes/includes/class-wcv-shortcodes.php
@@ -309,30 +309,61 @@ class WCV_Shortcodes {
 	 * @param array $atts
 	 * @return string
 	 */
-	public static function top_rated_products( $atts ) {
+	 public static function top_rated_products( $atts ) {
+		global $woocommerce_loop;
+
 		extract( shortcode_atts( array(
 			'vendor'		=> '',
 			'per_page'      => '12',
 			'columns'       => '4',
 			'orderby'       => 'title',
-			'order'         => 'ASC'
+			'order'         => 'asc'
 		), $atts ) );
 
-		$atts = array_merge( array(
-			'limit'        => $per_page,
-			'author'	   => self::get_vendor( $vendor ),
-			'columns'      => $columns,
-			'orderby'      => $orderby,
-			'order'        => $order,
-			'category'     => '',
-			'cat_operator' => 'IN',
-		), (array) $atts );
+		$meta_query   = array();
+		$meta_query[] = WC()->query->visibility_meta_query();
+		$meta_query[] = WC()->query->stock_status_meta_query();
 
-		$shortcode = new WC_Shortcode_Products( $atts, 'top_rated_products' );
+		$args = array(
+			'post_type' 			=> 'product',
+			'author'				=> self::get_vendor($vendor),
+			'post_status' 			=> 'publish',
+			'ignore_sticky_posts'   => 1,
+			'orderby' 				=> $orderby,
+			'order'					=> $order,
+			'posts_per_page' 		=> $per_page,
+			'meta_query'			=> $meta_query,
+		);
 
-		return $shortcode->get_content();
+		ob_start();
+
+		add_filter( 'posts_clauses', array( 'WC_Shortcodes', 'order_by_rating_post_clauses' ) );
+
+		$products = new WP_Query( apply_filters( 'woocommerce_shortcode_products_query', $args, $atts ) );
+
+		remove_filter( 'posts_clauses', array( 'WC_Shortcodes', 'order_by_rating_post_clauses' ) );
+
+		$woocommerce_loop['columns'] = $columns;
+
+		if ( $products->have_posts() ) : ?>
+
+			<?php woocommerce_product_loop_start(); ?>
+
+				<?php while ( $products->have_posts() ) : $products->the_post(); ?>
+
+					<?php wc_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php woocommerce_product_loop_end(); ?>
+
+		<?php endif;
+
+		wp_reset_postdata();
+
+		return '<div class="woocommerce columns-' . $columns . '">' . ob_get_clean() . '</div>';
 	}
-
+	
 	/**
 	 * List best selling products on sale per vendor
 	 *
