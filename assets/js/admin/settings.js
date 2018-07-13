5a4f01df7a54e6420088c70a32c2785c910966df
digitalchild <digitalchild@gmail.com>
2018-07-13 17:29:18 +0700

    Fixed: Color pickers in settings not working

--- assets/js/admin/settings.js
+++ assets/js/admin/settings.js
@@ -37,4 +37,38 @@
 		return false;
 	});
 
+	// Color picker
+	$( '.colorpick' )
+
+		.iris({
+			change: function( event, ui ) {
+				$( this ).parent().find( '.colorpickpreview' ).css({ backgroundColor: ui.color.toString() });
+			},
+			hide: true,
+			border: true
+		})
+
+		.on( 'click focus', function( event ) {
+			event.stopPropagation();
+			$( '.iris-picker' ).hide();
+			$( this ).closest( 'td' ).find( '.iris-picker' ).show();
+			$( this ).data( 'original-value', $( this ).val() );
+		})
+
+		.on( 'change', function() {
+			if ( $( this ).is( '.iris-error' ) ) {
+				var original_value = $( this ).data( 'original-value' );
+
+				if ( original_value.match( /^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/ ) ) {
+					$( this ).val( $( this ).data( 'original-value' ) ).change();
+				} else {
+					$( this ).val( '' ).change();
+				}
+			}
+		});
+
+	$( 'body' ).on( 'click', function() {
+		$( '.iris-picker' ).hide();
+	});
+
 })( jQuery );
