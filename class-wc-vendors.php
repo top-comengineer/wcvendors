b548ef1a08013a73773f8a660e0b6ab2afcad13d
dinhtungdu <dinhtungdu@gmail.com>
2019-07-04 16:16:40 +0700

    Update tested to

--- class-wc-vendors.php
+++ class-wc-vendors.php
@@ -11,7 +11,7 @@
  * Requires at least:    5.0.0
  * Tested up to:         5.2.2
  * WC requires at least: 3.3.0
- * WC tested up to:      3.6.4
+ * WC tested up to:      3.6.5
  *
  * Text Domain:          wc-vendors
  * Domain Path:          /languages/
